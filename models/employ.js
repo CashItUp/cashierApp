04dbd643b2f21b1d8a25b54a78a63f7d122b39cb
paulrld <dionespaul@gmail.com>
2018-02-01 23:30:35 -0800

    delete

--- models/employ.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Employee = sequelize.define("Employee", {
-  
-    idNumber: {type:DataTypes.INTEGER},
-    name: {type: DataTypes.STRING}
-});
-  return Employee;
-};
